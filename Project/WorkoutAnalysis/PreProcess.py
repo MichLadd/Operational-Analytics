25ad57374d4983b1dc9492669d6cd5f5a68d404c
MichLadd <95909039+MichLadd@users.noreply.github.com>
2024-09-22 17:27:38 +0200

    comments translated to eng

--- Project/WorkoutAnalysis/PreProcess.py
+++ Project/WorkoutAnalysis/PreProcess.py
@@ -1,39 +1,39 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Carica il dataset
+# Load the dataset
 df = pd.read_csv('./WorkoutData/workout_dataset_split.csv', sep=",")
 
-# Converti la colonna Date in datetime
+# Convert the 'Date' column to datetime
 df['Date'] = pd.to_datetime(df['Date'], dayfirst=True, errors='coerce')
 
-# Rimuovi le righe con valori NaT nella colonna 'Date'
+# Remove rows with NaT values in the 'Date' column
 df.dropna(subset=['Date'], inplace=True)
 
-# Imposta la colonna 'Date' come indice
+# Set the 'Date' column as the index
 df.set_index('Date', inplace=True)
 
-# Converti le colonne 'Weight (kg)', 'Sets', 'Reps' in numerico
+# Convert the 'Weight (kg)', 'Sets', 'Reps' columns to numeric
 df['Weight'] = pd.to_numeric(df['Weight'], errors='coerce')
 df['Sets'] = pd.to_numeric(df['Sets'], errors='coerce').fillna(4)
 df['Reps'] = pd.to_numeric(df['Reps'], errors='coerce').fillna(12)
 
-# Rimuovi le righe con valori NaN
+# Remove rows with NaN values
 df.dropna(subset=['Weight', 'Sets', 'Reps'], inplace=True)
 
-# Calcola il tonnellaggio (peso totale sollevato) per ogni esercizio
-df['tonnellaggio'] = df['Weight'] * df['Sets'] * df['Reps']
+# Calculate the tonnage (total weight lifted) for each exercise
+df['tonnage'] = df['Weight'] * df['Sets'] * df['Reps']
 
-# Aggregazione settimanale del tonnellaggio per gruppo muscolare
-weekly_data = df.groupby([pd.Grouper(freq='W'), 'Exercise']).agg({'tonnellaggio': 'sum'}).reset_index()
+# Weekly aggregation of tonnage by muscle group
+weekly_data = df.groupby([pd.Grouper(freq='W'), 'Exercise']).agg({'tonnage': 'sum'}).reset_index()
 
-# Pivot per avere i gruppi muscolari come colonne
-weekly_data_pivot = weekly_data.pivot(index='Date', columns='Exercise', values='tonnellaggio').fillna(0)
+# Pivot to have muscle groups as columns
+weekly_data_pivot = weekly_data.pivot(index='Date', columns='Exercise', values='tonnage').fillna(0)
 
-# Salva il dataset preprocessato
+# Save the preprocessed dataset
 weekly_data_pivot.to_csv('./WorkoutData/preprocessed_data.csv')
 
-# Plot dell'andamento del tonnellaggio per gruppo muscolare per avere un'idea della distribuzione del lavoro
+# Plot the trend of tonnage by muscle group to get an idea of the workload distribution
 '''
 plt.figure(figsize=(12, 6))
 for muscle_group in weekly_data_pivot.columns:
